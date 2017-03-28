3ee451131b374d5c429c013dae2a4f98c053bec2
zhucebuliaolongchuan <cl4076@nyu.edu>
2017-03-27 20:23:04 -0400

    updated on 2017-03-27 20:23:04

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 time_spot=`date "+%Y-%m-%d %H:%M:%S"`
 git remote remove origin
 git remote add origin https://github.com/zhucebuliaolongchuan/DataScienceCourses_Hw.git
-git pull lclc master --allow-unrelated-histories
+git pull origin master --allow-unrelated-histories
 git add *
 git commit -m "updated on ${time_spot}"
-git push lclc master
+git push origin master
